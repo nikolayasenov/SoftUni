f8f6e5928506ae1f9fa976a4c19a9e3d65228b4d
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-02-26 20:23:26 +0000

    initial commit

---
+++ Programming Basics With C#/01.FirstStepsInCoding-Lab/07.ProjectsCreation/ProjectsCreation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07.ProjectsCreation
+{
+    internal class ProjectsCreation
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int projects= int.Parse(Console.ReadLine());
+            int hoursNeeded = projects * 3;
+            Console.WriteLine($"The architect {architectName} will need {hoursNeeded} hours to complete {projects} project/s.");
+        }
+    }
+}
