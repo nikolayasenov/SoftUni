f5ac696b818bce9506616748d48a56a6a07f8d3a
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-02-26 20:25:06 +0000

    initial commit

---
+++ Programming Basics With C#/01.FirstStepsInCoding-Lab/08.PetShop/PetShop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    internal class PetShop
+    {
+        static void Main(string[] args)
+        {
+            int dogFoodBags = int.Parse(Console.ReadLine());
+            int catFoodBags = int.Parse(Console.ReadLine());
+            double finalPrice = dogFoodBags * 2.50 + catFoodBags * 4;
+
+            Console.WriteLine($"{finalPrice} lv.");
+        }
+    }
+}
