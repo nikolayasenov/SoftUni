64a77cffa9ccd8d46faa6d7ef0311b89764d99ba
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-02-26 20:06:25 +0000

    initial commit

---
+++ Programming Basics With C#/01.FirstStepsInCoding-Lab/04.InchesToCentimeters/InchesToCentimeters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04.InchesToCentimeters
+{
+    internal class InchesToCentimeters
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine(centimeters);
+        }
+    }
+}
