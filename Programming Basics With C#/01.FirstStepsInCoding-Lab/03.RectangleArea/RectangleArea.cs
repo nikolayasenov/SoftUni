a2725a51acb01919309d1fd27fe184e79a5394ca
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-02-26 20:05:23 +0000

    initial commit

---
+++ Programming Basics With C#/01.FirstStepsInCoding-Lab/03.RectangleArea/RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.RectangleArea
+{
+    internal class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int square = a * b;
+            Console.WriteLine(square);
+        }
+    }
+}
