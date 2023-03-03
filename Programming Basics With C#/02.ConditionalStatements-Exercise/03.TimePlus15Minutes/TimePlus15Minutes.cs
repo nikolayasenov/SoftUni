dc128e308a0f6498e90591af4e11c6a86c69abc1
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-03 22:23:24 +0000

    ïnitial commit

---
+++ Programming Basics With C#/02.ConditionalStatements-Exercise/03.TimePlus15Minutes/TimePlus15Minutes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03.TimePlus15Minutes
+{
+    internal class TimePlus15Minutes
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 15;
+            if (minutes >= 60)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+
+            if (minutes < 10)
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+            
+        }
+    }
+}
