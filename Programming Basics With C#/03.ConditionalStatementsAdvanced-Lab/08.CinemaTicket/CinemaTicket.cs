e2b70321399ccfb19ebf9d1233ede17a3808b7ce
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-06 18:42:31 +0000

    initial commit

---
+++ Programming Basics With C#/03.ConditionalStatementsAdvanced-Lab/08.CinemaTicket/CinemaTicket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08.CinemaTicket
+{
+    internal class CinemaTicket
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int priceOfTicket = 0;
+            if (day == "Monday" || day == "Tuesday" || day == "Friday")
+            {
+                priceOfTicket = 12;
+            }
+            else if (day == "Wednesday" || day == "Thursday")
+            {
+                priceOfTicket = 14;
+            }
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                priceOfTicket = 16;
+            }
+            Console.WriteLine(priceOfTicket);
+        }
+    }
+}
