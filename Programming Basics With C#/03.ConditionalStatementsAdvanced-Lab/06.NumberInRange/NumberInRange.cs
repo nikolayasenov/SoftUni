f1d9ed0113253a2bcde97da4d36df6a26820d768
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-06 18:41:23 +0000

    initial commit

---
+++ Programming Basics With C#/03.ConditionalStatementsAdvanced-Lab/06.NumberInRange/NumberInRange.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.NumberInRange
+{
+    internal class NumberInRange
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number <= 100 && number >= -100 && number != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+
+        }
+    }
+}
