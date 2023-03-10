cd7e65ef6d9f811141e8aac621c0bfc72a40709f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-10 19:36:08 +0000

    initial commit

---
+++ Programming Basics With C#/03.ConditionalStatementsAdvanced-Exercise/02.SummerOutfit/SummerOutfit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _02.SummerOutfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = int.Parse(Console.ReadLine());
+            string timeOfTheDay = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+
+            if (temp <= 18)
+            {
+                if (timeOfTheDay == "Morning")
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (timeOfTheDay == "Afternoon" || timeOfTheDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            else if (temp <= 24)
+            {
+                if (timeOfTheDay == "Morning" || timeOfTheDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (timeOfTheDay == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else
+            {
+                if (timeOfTheDay == "Morning")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfTheDay == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                else if (timeOfTheDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            Console.WriteLine($"It's {temp} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
