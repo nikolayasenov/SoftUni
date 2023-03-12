b8c8752b867a625316a8b3e1bc19614009f1bbdd
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-12 21:12:11 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Lab/02.NumbersNToOne/NumbersNToOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02.NumbersNToOne
+{
+    internal class NumbersNToOne
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = number; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
