566df87222168df9f462bf80634311f421162a29
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-13 21:48:11 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Lab/06.VowelsSum/VowelsSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06.VowelsSum
+{
+    internal class VowelsSum
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int result = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentLetter = word[i];
+                switch (currentLetter)
+                {
+                    case 'a':
+                        result += 1;
+                        break;
+                    case 'e':
+                        result += 2;
+                        break;
+                    case 'i':
+                        result += 3;
+                        break;
+                    case 'o':
+                        result += 4;
+                        break;
+                    case 'u':
+                        result += 5;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
