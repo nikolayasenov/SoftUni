1198032aeb17830a52a021a31b3f0dfa1023ad15
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-14 22:07:31 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Lab/10.OddEvenSum/OddEvenSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _10.OddEvenSum
+{
+    internal class OddEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int resultEven = 0;
+            int resultOdd = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    resultEven += currentNumber;
+                }
+                else
+                {
+                    resultOdd += currentNumber;
+                }
+    
+            }
+            if (resultEven == resultOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {resultEven}");
+            }
+            else
+            {
+                int diff = Math.Abs(resultEven - resultOdd);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
