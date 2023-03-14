0863c6b6f5addbf06477f570712d516235e04997
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-14 22:07:02 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Lab/09.LeftAndRightSum/LeftAndRightSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09.LeftAndRightSum
+{
+    internal class LeftAndRightSum
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int resultOne = 0;
+            int resultTwo = 0;
+            int difference = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                resultOne += currentNumber;
+            }
+            for (int i = 0; i < numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                resultTwo += currentNumber;
+            }
+            if (resultOne == resultTwo)
+            {
+                Console.WriteLine($"Yes, sum = {resultOne}");
+            }
+            else
+            {
+                difference = Math.Abs(resultOne - resultTwo);
+                Console.WriteLine($"No, diff = {difference}");
+            }
+        }
+    }
+}
