2b3bd20a677afc683ad1a57a2dc92ff6707ab0f4
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-14 22:05:52 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Lab/07.SumNumbers/SumNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _07.SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                result += currentNumber;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
