00c900b2bb2237a478580b3fc9d0491a0b3ddd55
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-14 22:06:17 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Lab/08.NumberSequence/NumberSequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    internal class NumberSequence
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+            for (int i = 0; i < numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
