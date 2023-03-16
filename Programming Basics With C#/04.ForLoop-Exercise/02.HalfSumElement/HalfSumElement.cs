67aef1db6fc8be3564a817a11ff57f7e256e6365
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-16 18:58:38 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Exercise/02.HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    internal class HalfSumElement
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber = int.MinValue;
+            for (int i = 0; i < numbers; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+                if (currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+            int sumWithoutMaxNumber = sum - maxNumber;
+            if (maxNumber == sumWithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+                int difference = Math.Abs(maxNumber - sumWithoutMaxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {difference}");
+            }
+        }
+    }
+}
