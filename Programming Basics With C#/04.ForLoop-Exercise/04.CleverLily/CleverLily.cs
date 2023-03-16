3958a7c77b58cd754a1b42dc650adc2c391440dd
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-16 18:59:47 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Exercise/04.CleverLily/CleverLily.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.CleverLily
+{
+    internal class CleverLily
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int singleToyPrice = int.Parse(Console.ReadLine());
+            int savedMoney = 0;
+            int giftedMoney = 10;
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    savedMoney += singleToyPrice;
+
+                }
+                else
+                {
+                    savedMoney += giftedMoney - 1;
+                    giftedMoney += 10;
+                }
+            }
+            double diff = Math.Abs(washingMachinePrice - savedMoney);
+            if (savedMoney >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {diff:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {diff:f2}");
+            }
+        }
+    }
+}
