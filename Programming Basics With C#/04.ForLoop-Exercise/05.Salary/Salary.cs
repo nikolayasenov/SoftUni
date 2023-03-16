bb911ffa187e30d073c6fa48558fd9f13dd324d5
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-16 19:00:20 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Exercise/05.Salary/Salary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05.Salary
+{
+    internal class Salary
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            int fee = 0;
+            for (int i = 0; i < tabs && fee < salary; i++)
+            {
+
+                string openTab = Console.ReadLine();
+                if (openTab == "Facebook")
+                {
+                    fee += 150;
+                }
+                else if (openTab == "Instagram")
+                {
+                   fee += 100;
+                }
+                else if (openTab == "Reddit")
+                {
+                    fee += 50;
+                }
+            }
+            if (fee >= salary)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary - fee);
+            }
+
+        }
+    }
+}
