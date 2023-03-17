18116a07adc8d1e90cb67ba10eff8eb468cc7354
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-17 21:45:45 +0000

    initial commit

---
+++ Programming Basics With C#/04.ForLoop-Exercise/08.TennisRanklist/TennisRanklist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _08.TennisRanklist
+{
+    internal class TennisRanklist
+    {
+        static void Main(string[] args)
+        {
+            int tournaments = int.Parse(Console.ReadLine());
+            int startingPoints = int.Parse(Console.ReadLine());
+            int points = 0;
+            int wins = 0;
+            for (int i = 0; i < tournaments; i++)
+            {
+                string rank = Console.ReadLine();
+                if (rank == "W")
+                {
+                    points += 2000;
+                    wins++;
+                }
+                else if(rank == "F")
+                {
+                    points += 1200;
+                }
+                else if (rank == "SF")
+                {
+                    points += 720;
+                }
+            }
+            int finalPoints = points + startingPoints;
+            double averagePoints = points / tournaments; //Math.Floor
+            double wonTournamentsPercentage = (double)wins / tournaments * 100;
+            Console.WriteLine($"Final points: {finalPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{wonTournamentsPercentage:f2}%");
+        }
+    }
+}
