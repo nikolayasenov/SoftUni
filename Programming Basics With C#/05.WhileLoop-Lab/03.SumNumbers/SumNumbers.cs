b5b0cef9be7d0d036095a53fb183fefc1709ceb2
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-20 20:14:21 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Lab/03.SumNumbers/SumNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.SumNumbers
+{
+    internal class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (number > sum)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                sum += numbers;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
