8fa1bb65c2e35d2dd290f5b037f45b0d5f1f159d
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-20 20:16:23 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Lab/05.AccountBalance/AccountBalance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    internal class AccountBalance
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double balance = 0;
+            while (input != "NoMoreMoney")
+            {
+                double deposit = double.Parse(input);
+                if (deposit < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                balance += deposit;
+                Console.WriteLine($"Increase: {deposit:f2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {balance:f2}");
+        }
+    }
+}
