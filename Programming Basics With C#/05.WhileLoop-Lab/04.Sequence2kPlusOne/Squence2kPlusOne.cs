4ff62c5725fe1eba425ec53311f249582f6859ec
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-20 20:15:24 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Lab/04.Sequence2kPlusOne/Squence2kPlusOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04.Sequence2kPlusOne
+{
+    internal class Squence2kPlusOne
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int result = 1;
+            while (result <= number)
+            {
+                Console.WriteLine(result);
+                result = result * 2 + 1;
+            }
+        }
+    }
+}
