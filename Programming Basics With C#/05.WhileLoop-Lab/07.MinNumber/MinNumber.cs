06faec2e0b26484b9ed54cf0a140e10185b24aaf
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-21 20:09:22 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Lab/07.MinNumber/MinNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    internal class MinNumber
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int min = int.MaxValue;
+            while (input != "Stop")
+            {
+                int currentNumber = int.Parse(input);
+                if (currentNumber < min)
+                {
+                    min = currentNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
