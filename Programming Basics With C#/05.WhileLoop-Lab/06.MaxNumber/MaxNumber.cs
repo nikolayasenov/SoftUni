4c17400c613023f131e09ecf385ec10fb4b6378e
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-21 20:08:34 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Lab/06.MaxNumber/MaxNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    internal class MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = int.MinValue;
+            while (input != "Stop")
+            {
+                int currentNumber = int.Parse(input);
+                if (currentNumber > max)
+                {
+                    max = currentNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
