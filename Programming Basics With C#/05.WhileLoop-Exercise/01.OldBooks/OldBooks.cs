9af8a865fe6e007e32dbb9f97841f2dff9476f23
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-23 21:07:38 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Exercise/01.OldBooks/OldBooks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    internal class OldBooks
+    {
+        static void Main(string[] args)
+        {
+            string bookToLookFor = Console.ReadLine();
+            string currentBook = Console.ReadLine();
+            bool isFound = false;
+            int bookCount = 0;
+            while (currentBook != "No More Books")
+            {
+
+                if (currentBook == bookToLookFor)
+                {
+                    isFound = true;
+                    break;
+                }
+                currentBook = Console.ReadLine();
+                bookCount++;
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"You checked {bookCount} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {bookCount} books.");
+            }
+        }
+    }
+}
