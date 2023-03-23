e51c0d18e7e9ed2be494e5e5393bfa9bfdc6eadd
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-23 21:10:00 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Exercise/04.Walking/Walking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04.Walking
+{
+    internal class Walking
+    {
+        static void Main(string[] args)
+        {
+            const int stepsThreshold = 10000;
+            int sumOfSteps = 0;
+            bool goalReached = false;
+            while (!goalReached && sumOfSteps < stepsThreshold)
+            {
+                string input = Console.ReadLine();
+                int currentSteps;
+                if (input == "Going home")
+                {
+                    currentSteps = int.Parse(Console.ReadLine());
+                    goalReached = true;
+                }
+                else
+                {
+                    currentSteps = int.Parse(input);
+                }
+                sumOfSteps += currentSteps;
+            }
+            int difference = Math.Abs(stepsThreshold - sumOfSteps);
+            if (sumOfSteps >= stepsThreshold)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{difference} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{difference} more steps to reach goal.");
+            }
+        }
+    }
+}
