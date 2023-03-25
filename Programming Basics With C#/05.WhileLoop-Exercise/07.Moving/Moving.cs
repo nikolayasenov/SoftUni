8cee45554226c5df106d76784123314b221d697b
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-25 08:52:29 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Exercise/07.Moving/Moving.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07.Moving
+{
+    internal class Moving
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int freeSpace = width * lenght * height;
+            bool movingOver = false;
+            while (!movingOver && freeSpace > 0)
+            {
+                string input = Console.ReadLine();
+                int numberOfBoxes;
+                if (input == "Done")
+                {
+                    movingOver = true;
+                    break;
+                }
+                else
+                {
+                    numberOfBoxes = int.Parse(input);
+                    freeSpace -= numberOfBoxes;
+                }
+            }
+            int cubicMeters = Math.Abs(freeSpace);
+            if (movingOver && cubicMeters > 0)
+            {
+                Console.WriteLine($"{freeSpace} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {cubicMeters} Cubic meters more.");
+            }
+        }
+    }
+}
