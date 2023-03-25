4f61c32db10977e0dcb7c09afb6553f3804cb5d1
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-25 08:51:52 +0000

    initial commit

---
+++ Programming Basics With C#/05.WhileLoop-Exercise/06.Cake/Cake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    internal class Cake
+    {
+        static void Main(string[] args)
+        {
+            int cakeLenght = int.Parse(Console.ReadLine());
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakePieces = cakeLenght * cakeWidth;
+            bool stopEating = false;
+            while (!stopEating && cakePieces > 0)
+            {
+                string input = Console.ReadLine();
+                int takenPieces;
+                if (input == "STOP")
+                {
+                    stopEating = true;
+                    break;
+                }
+                else 
+                {
+                    takenPieces = int.Parse(input);
+                    cakePieces -= takenPieces;
+                }
+            }
+            int piecesLeft = Math.Abs(cakePieces);
+            if (stopEating && piecesLeft > 0)
+            {
+                Console.WriteLine($"{piecesLeft} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {piecesLeft} pieces more.");
+            }
+        }
+    }
+}
