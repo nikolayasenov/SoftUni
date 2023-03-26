f129b6f23a7d4721d7e1a6b0de30743bda56a4b3
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-26 22:33:57 +0100

    initial commit

---
+++ Programming Basics With C#/06.NestedLoops-Lab/05.Traveling/Traveling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.Traveling
+{
+    internal class Traveling
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                while (budget > 0)
+                {
+                    double currentSavedMoney = double.Parse(Console.ReadLine());
+                    budget -= currentSavedMoney;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+
+        }
+    }
+}
