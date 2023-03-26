f0787f0027b190e0dc6c5d0039d75dcd17e4edff
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-26 22:33:00 +0100

    initial commit

---
+++ Programming Basics With C#/06.NestedLoops-Lab/03.Combinations/Combinations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.Combinations
+{
+    internal class Combinations
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int validCombinations = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k <= n; k++)
+                    {
+                        if (i + j + k == n)
+                        {
+                            validCombinations++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validCombinations);
+        }
+    }
+}
