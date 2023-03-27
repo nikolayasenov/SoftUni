8eb3605c08232306afb51a63e58e649015552a65
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-27 22:47:21 +0100

    initial commit

---
+++ Programming Basics With C#/06.NestedLoops-Exercise/01.NumberPyramid/NumberPyramid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01.NumberPyramid
+{
+    internal class NumberPyramid
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int row = 1;
+            int numberToPrint = 1;
+            while (numberToPrint <= n)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    Console.Write($"{numberToPrint} ");
+                    numberToPrint++;
+                    if (numberToPrint > n)
+                    {
+                        break;
+                    }
+                }
+                row++;
+                Console.WriteLine();
+            }
+            //int n = int.Parse(Console.ReadLine());
+            //int row = 1;
+            //int countOfWrittenNumbers = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    Console.Write($"{i} ");
+            //    countOfWrittenNumbers++;
+            //    if (countOfWrittenNumbers == row)
+            //    {
+            //        Console.WriteLine();
+            //        countOfWrittenNumbers = 0;
+            //        row++;
+            //    }
+            //}
+        }
+    }
+}
