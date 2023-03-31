6c5efaf8b2129f1766a13bb1e78175dbce588419
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-03-31 16:40:10 +0100

    initial commit

---
+++ Programming Basics With C#/07.PBCSharp-Exam/01.CatDiet/CatDiet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.CatDiet
+{
+    internal class CatDiet
+    {
+        static void Main(string[] args)
+        {
+            int fatPercentage = int.Parse(Console.ReadLine());
+            int proteinPercentage = int.Parse(Console.ReadLine());
+            int carboPercentage = int.Parse(Console.ReadLine());
+            int requiredCalories = int.Parse(Console.ReadLine());
+            int waterPercentage = int.Parse(Console.ReadLine());
+
+            double fatCal = (fatPercentage * requiredCalories * 0.01) / 9.0;
+            double proteinCal = (proteinPercentage * requiredCalories * 0.01) / 4.0;
+            double carboCal = (carboPercentage * requiredCalories * 0.01) / 4.0;
+
+            double totalFood = fatCal + proteinCal + carboCal;
+            double foodCalPerGram = requiredCalories / totalFood;
+            double calWithoutWater = foodCalPerGram * waterPercentage * 0.01;
+            double finalCal = foodCalPerGram - calWithoutWater;
+            Console.WriteLine($"{finalCal:f4}");
+
+
+        }
+    }
+}
