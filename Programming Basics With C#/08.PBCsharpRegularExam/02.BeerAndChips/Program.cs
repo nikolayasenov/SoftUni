acaf47b1d7b47a69d9bad59498344d90860384fa
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-14 10:05:21 +0100

    initial commit

---
+++ Programming Basics With C#/08.PBCsharpRegularExam/02.BeerAndChips/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.BeerAndChips
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string footballFanName = Console.ReadLine();
+            double budget = double.Parse(Console.ReadLine());
+            int beerBottles = int.Parse(Console.ReadLine());
+            int packetsChips = int.Parse(Console.ReadLine());
+
+            double beerPrice = beerBottles * 1.20;
+            double chipsPrice = (beerPrice * 0.45) * packetsChips;
+            double orderPrice = Math.Ceiling(chipsPrice) + beerPrice;
+            double moneyDiff = Math.Abs(budget - orderPrice);
+            if (orderPrice <= budget)
+            {
+                Console.WriteLine($"{footballFanName} bought a snack and has {moneyDiff:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"{footballFanName} needs {moneyDiff:f2} more leva!");
+            }
+        }
+    }
+}
