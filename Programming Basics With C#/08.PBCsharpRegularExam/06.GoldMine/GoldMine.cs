be174b0ac91ec1e5553b7c9bb4840cbcd02271a6
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-14 10:11:39 +0100

    initial commit

---
+++ Programming Basics With C#/08.PBCsharpRegularExam/06.GoldMine/GoldMine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.GoldMine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLocations = int.Parse(Console.ReadLine());
+            double averageGold = 0;
+            double totalGoldPerLocation = 0;
+            for (int i = numberOfLocations; i > 0; i--)
+            {
+                totalGoldPerLocation = 0;
+                double dailyProjectedAverageGold = double.Parse(Console.ReadLine());
+                int daysDigging = int.Parse(Console.ReadLine());
+                int firstDaysDigging = daysDigging;
+                while (daysDigging > 0)
+                {
+                    double dailyDiggedGold = double.Parse(Console.ReadLine());
+                    totalGoldPerLocation += dailyDiggedGold;
+                    daysDigging--;
+                }
+                averageGold = totalGoldPerLocation / firstDaysDigging;
+                if (averageGold >= dailyProjectedAverageGold)
+                {
+                    Console.WriteLine($"Good job! Average gold per day: {averageGold:f2}.");
+                }
+                else
+                {
+                    double goldDiff = Math.Abs(dailyProjectedAverageGold - averageGold);
+                    Console.WriteLine($"You need {goldDiff:f2} gold.");
+                }
+            }
+        }
+    }
+}
