46025d62796abdbf979eebead5981803078b7b94
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-16 19:49:25 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/02.Passed/Passed.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.Passed
+{
+    internal class Passed
+    {
+        static void Main(string[] args)
+        {
+            float grade = float.Parse(Console.ReadLine()!);
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
