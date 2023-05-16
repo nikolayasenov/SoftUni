b5a8909f322ab5423df677819389e6447a47aa2c
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-16 20:23:48 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/03.PassedOrFailed/PassedOrFailed.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.PassedOrFailed
+{
+    internal class PassedOrFailed
+    {
+        static void Main(string[] args)
+        {
+            float grade = float.Parse(Console.ReadLine()!);
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
