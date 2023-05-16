a6040d805978a7ff45d2d2d5c73ecaa8be1f6884
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-16 14:44:07 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntaxConditionalStatementsAndLoops-Lab/01.StudentInformation/StudentInformation.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.StudentInformation
+{
+    internal class StudentInformation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
