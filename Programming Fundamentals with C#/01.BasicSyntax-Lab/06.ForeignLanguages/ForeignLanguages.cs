c19c8ff8513a63430f8ff47975eeaec6e378ecb4
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-25 21:38:58 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/06.ForeignLanguages/ForeignLanguages.cs
@@ -0,0 +1,25 @@
+﻿namespace _06.ForeignLanguages
+{
+    internal class ForeignLanguages
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Argentina":
+                case "Spain":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
