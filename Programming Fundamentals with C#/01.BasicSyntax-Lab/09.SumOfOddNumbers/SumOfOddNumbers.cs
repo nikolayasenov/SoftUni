835121305300c777e22a3a074dbaf507da5a758a
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-25 21:40:14 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/09.SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace _09.SumOfOddNumbers
+{
+    internal class SumOfOddNumbers
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= rows * 2; i += 2)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+
+            //int countOfNumbers = int.Parse(Console.ReadLine());
+            //int sumOfOddNumbers = 0;
+            //int counter = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //        sumOfOddNumbers += i;
+            //        counter++;
+            //    }
+            //    if (counter == countOfNumbers)
+            //    {
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine($"Sum: {sumOfOddNumbers}");
+        }
+    }
+}
