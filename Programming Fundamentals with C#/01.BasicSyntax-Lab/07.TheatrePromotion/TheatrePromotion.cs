44da6632c3d3844a5820d14b2c1d32df29d10043
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-25 21:39:44 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/07.TheatrePromotion/TheatrePromotion.cs
@@ -0,0 +1,58 @@
+﻿namespace _07.TheatrePromotion
+{
+    internal class TheatrePromotion
+    {
+        static void Main(string[] args)
+        {
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+
+            if (typeOfDay == "Weekday")
+            {
+                if (age >= 0 && age <= 18 || age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 18;
+                }
+            }
+            else if (typeOfDay == "Weekend")
+            {
+                if (age >= 0 && age <= 18 || age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+            }
+            else if (typeOfDay == "Holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+            }
+            if (price > 0)
+            {
+                Console.WriteLine($"{price}$");
+            }
+            else 
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
