09246422210ab85faba6ba9935599bbb7c71769f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-25 21:41:23 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/08.DivisibleBy3/DivisibleBy3.cs
@@ -0,0 +1,13 @@
+﻿namespace _08.DivisibleBy3
+{
+    internal class DivisibleBy3
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 100; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
