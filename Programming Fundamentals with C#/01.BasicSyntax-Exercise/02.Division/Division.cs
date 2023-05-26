78eab5e3a883940821618e573a7ffc74b8dbce3f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-26 21:42:56 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Exercise/02.Division/Division.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int biggestDivider = 0;
+            if (number % 10 == 0)
+            {
+                biggestDivider = 10;
+            }
+            else if (number % 7 == 0)
+            {
+                biggestDivider = 7;
+            }
+            else if (number % 6 == 0)
+            {
+                biggestDivider = 6;
+            }
+            else if (number % 3 == 0)
+            {
+                biggestDivider = 3;
+            }
+            else if (number % 2 == 0)
+            {
+                biggestDivider = 2;
+            }
+            if (biggestDivider > 0)
+            {
+                Console.WriteLine($"The number is divisible by {biggestDivider}");
+            }
+            else
+            {
+                Console.WriteLine("Not divisible");
+            }
+        }
+    }
+}
