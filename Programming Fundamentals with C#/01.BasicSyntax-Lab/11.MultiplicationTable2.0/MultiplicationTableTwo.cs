16a0b1de3d21f4490ec9b9ce1fc0b2df9e1ebb14
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-26 21:39:06 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Lab/11.MultiplicationTable2.0/MultiplicationTableTwo.cs
@@ -0,0 +1,24 @@
+﻿namespace _11.MultiplicationTable2._0
+{
+    internal class MultiplicationTableTwo
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            if (multiplier > 10)
+            {
+                int result = number * multiplier;
+                Console.WriteLine($"{number} X {multiplier} = {result}");
+            }
+            else if (true)
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    int product = number * i;
+                    Console.WriteLine($"{number} X {i} = {product}");
+                }
+            }
+        }
+    }
+}
