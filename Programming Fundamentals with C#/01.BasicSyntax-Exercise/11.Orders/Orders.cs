1a2c3f023d4afb5cd1ec82e3e50c5f1201052f0d
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-27 20:09:59 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Exercise/11.Orders/Orders.cs
@@ -0,0 +1,23 @@
+﻿namespace _11.Orders
+{
+    internal class Orders
+    {
+        static void Main(string[] args)
+        {
+            int ordersCount = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            while (ordersCount > 0)
+            {
+                double price = 0;
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+                price = (days * capsulesCount) * pricePerCapsule;
+                totalPrice += price;
+                ordersCount--;
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
