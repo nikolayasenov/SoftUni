4fab440126e849a0c1f28bc509ad9524b6f80644
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-27 20:05:13 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Exercise/09.PadawanEquipment/PadawanEquipment.cs
@@ -0,0 +1,30 @@
+﻿namespace _09.PadawanEquipment
+{
+    internal class PadawanEquipment
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = float.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            double totalLightsabersPrice = (Math.Ceiling(students * 1.10)) * lightsaberPrice;
+            double totalRobePrice = students * robePrice;
+            double totalBeltPrice = (students - (students / 6)) * beltPrice;
+
+            totalPrice = totalLightsabersPrice + totalRobePrice + totalBeltPrice;
+            double neededMoney = Math.Abs(totalPrice - money);
+            if (money >= totalPrice)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {neededMoney:f2}lv more.");
+            }
+
+        }
+    }
+}
