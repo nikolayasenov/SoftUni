c6e4431cf0d15e470b030b7413ef571454bb440b
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-27 20:03:55 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Exercise/08.TriangleOfNumbers/TriangleOfNumbers.cs
@@ -0,0 +1,18 @@
+﻿namespace _08.TriangleOfNumbers
+{
+    internal class TriangleOfNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= number; row++)
+            {
+                for (int times = 1; times <= row; times++)
+                {
+                    Console.Write(row + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
