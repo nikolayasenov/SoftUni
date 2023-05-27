064a3a0d93fb827ee7bd83ad6842993d3f50fd50
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-27 20:02:21 +0100

    initial commit

---
+++ Programming Fundamentals with C#/01.BasicSyntax-Exercise/06.StrongNumber/StrongNumber.cs
@@ -0,0 +1,41 @@
+﻿namespace _06.StrongNumber
+{
+    internal class StrongNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string numberInText = number.ToString();
+            int sum = 0;
+            for (int i = 0; i < numberInText.Length; i++)
+            {
+                int result = 0;
+                double currentDigitInDouble = char.GetNumericValue(numberInText[i]);
+                int currentDigit = (int)currentDigitInDouble;
+                if (currentDigit != 0 && currentDigit != 1)
+                {
+                    for (int factoriel = currentDigit - 1; factoriel >= 1; factoriel--)
+                    {
+                        result = currentDigit * factoriel;
+                        currentDigit = result;
+                    }
+
+                    sum += result;
+                }
+                else
+                {
+                    sum += 1;
+                }
+
+            }
+            if (number == sum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
