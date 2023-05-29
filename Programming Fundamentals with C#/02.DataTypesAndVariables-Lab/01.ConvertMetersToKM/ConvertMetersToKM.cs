1327b503401eb804a7884ace557e7f9c54a817ec
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-29 21:02:10 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/01.ConvertMetersToKM/ConvertMetersToKM.cs
@@ -0,0 +1,12 @@
+﻿namespace _01.ConvertMetersToKM
+{
+    internal class ConvertMetersToKM
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double convert = number * 0.001;
+            Console.WriteLine($"{convert:f2}");
+        }
+    }
+}
