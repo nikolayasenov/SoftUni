152068e2432c497881441f3d92de09bc74311977
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-29 21:03:04 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/03.ExactSumOfRealNumbers/ExactSumOfRealNumbers.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.ExactSumOfRealNumbers
+{
+    internal class ExactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            while (numbersCount > 0)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+                numbersCount--;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
