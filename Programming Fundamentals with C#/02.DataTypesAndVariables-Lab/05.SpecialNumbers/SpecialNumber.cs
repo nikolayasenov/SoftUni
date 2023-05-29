5f6e6a4823d03aa3f09426dfe3fdc70f8d255d87
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-29 21:03:42 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/05.SpecialNumbers/SpecialNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.SpecialNumbers
+{
+    internal class SpecialNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                bool isSpecial = false;
+                int sum = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sum += digits % 10;
+                    digits /= 10;
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    isSpecial = true;
+                }
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
