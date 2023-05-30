352bb53bcf022854a41bd3c5c979bb2015901e00
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-30 21:19:33 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/07.ConcatNames/ConcatNames.cs
@@ -0,0 +1,13 @@
+﻿namespace _07.ConcatNames
+{
+    internal class ConcatNames
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = Console.ReadLine();
+            string nameTwo = Console.ReadLine();
+            string delimer = Console.ReadLine();
+            Console.WriteLine($"{nameOne}{delimer}{nameTwo}");
+        }
+    }
+}
