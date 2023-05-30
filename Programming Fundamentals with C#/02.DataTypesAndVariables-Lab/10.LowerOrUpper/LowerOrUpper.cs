de4208d8dba8572a253a4a3536ac3e68f33df301
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-30 21:20:43 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/10.LowerOrUpper/LowerOrUpper.cs
@@ -0,0 +1,19 @@
+﻿namespace _10.LowerOrUpper
+{
+    internal class LowerOrUpper
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            bool isUpper= char.IsUpper(input);
+            if (isUpper)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
