5a21e2119951b251f8f0285adea620f406ea81ca
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-30 21:21:33 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/12.RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace _12.RefactorSpecialNumbers
+{
+    internal class RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numbersCount; i++)
+            {
+                bool isSpecialNum = false;
+                int sum = 0;
+                int digit = i;
+                while (digit > 0)
+                {
+                    sum += digit % 10;
+                    digit = digit / 10;
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    isSpecialNum = true;
+                } 
+
+                Console.WriteLine($"{i} -> {isSpecialNum}");
+            }
+        }
+    }
+}
