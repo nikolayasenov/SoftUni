052263b82ea2b2a5f49d2e0b4f135e551354746f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-30 21:20:20 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/09.CharToString/CharToString.cs
@@ -0,0 +1,16 @@
+﻿namespace _09.CharToString
+{
+    internal class CharToString
+    {
+        static void Main(string[] args)
+        {
+            string text = String.Empty;
+            for (int i = 0; i < 3; i++)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                text += new string(currentChar.ToString());
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
