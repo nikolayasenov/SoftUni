dacad7c32f37e0fb3cb412c8d068df8019a79615
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-30 21:19:55 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Lab/08.TownInfo/TownInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace _08.TownInfo
+{
+    internal class TownInfo
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}
