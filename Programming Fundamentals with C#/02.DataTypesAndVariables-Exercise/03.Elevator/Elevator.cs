5194eef0b2de32020a1cf1ac7d9bbe41b004332f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-31 18:30:54 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/03.Elevator/Elevator.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.Elevator
+{
+    internal class Elevator
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int courses = 0;
+            // int courses = (int)Math.Celing((double)peopleCount / capacity)
+            if (peopleCount % capacity == 0)
+            {
+                courses = peopleCount / capacity;
+            }
+            else
+            {
+                int leftPersons = peopleCount % capacity;
+                courses = peopleCount / capacity + 1;
+            }
+            Console.WriteLine(courses);
+
+        }
+    }
+}
