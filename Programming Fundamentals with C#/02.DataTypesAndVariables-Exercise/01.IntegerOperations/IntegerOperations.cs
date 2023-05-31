0dca4ac2196fe1e7022eed6ae96b29a81094f73f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-31 18:30:00 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/01.IntegerOperations/IntegerOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.IntegerOperations
+{
+    internal class IntegerOperations
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            int numberFour = int.Parse(Console.ReadLine());
+
+            int addOneToTwo = numberOne + numberTwo;
+            int divideAddByThree = addOneToTwo / numberThree;
+            int multiplyDivideByFour = divideAddByThree * numberFour;
+            Console.WriteLine(multiplyDivideByFour);
+        }
+    }
+}
