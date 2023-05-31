0395dbf4cb2b24d331d7e54fc5ec977e00e4ba62
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-05-31 18:31:42 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/04.SumOfChars/SumOfChars.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.SumOfChars
+{
+    internal class SumOfChars
+    {
+        static void Main(string[] args)
+        {
+            byte numberOfLines = byte.Parse(Console.ReadLine());
+            double sum = 0;
+            while (numberOfLines > 0)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                sum += (int)currentChar;
+                numberOfLines--;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
