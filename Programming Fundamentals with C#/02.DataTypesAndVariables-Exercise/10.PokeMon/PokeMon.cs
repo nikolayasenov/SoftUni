36781b989fdc956747d6fc58ca870b376cc1a059
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-03 17:41:02 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/10.PokeMon/PokeMon.cs
@@ -0,0 +1,25 @@
+﻿namespace _10.PokeMon
+{
+    internal class PokeMon
+    {
+        static void Main(string[] args)
+        {
+            int originalPokePower = int.Parse(Console.ReadLine());
+            int pokePower = originalPokePower;
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int countPokedTargets = 0;
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                countPokedTargets++;
+                if (pokePower == originalPokePower * 0.5 && exhaustionFactor != 0)
+                {
+                    pokePower = pokePower / exhaustionFactor;
+                }
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(countPokedTargets);
+        }
+    }
+}
