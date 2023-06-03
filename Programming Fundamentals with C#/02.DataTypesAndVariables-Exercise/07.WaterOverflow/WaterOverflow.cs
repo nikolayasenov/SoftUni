bbfa2caf0631c649362e8ba4f3adab838f0de5e1
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-03 17:39:46 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/07.WaterOverflow/WaterOverflow.cs
@@ -0,0 +1,29 @@
+﻿namespace _07.WaterOverflow
+{
+    internal class WaterOverflow
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 255;
+            int litresInTheTank = 0;
+            int numberOfLines = int.Parse(Console.ReadLine());
+            while (numberOfLines > 0)
+            {
+                int litresOfWater = int.Parse(Console.ReadLine());
+                if (litresOfWater > capacity || litresOfWater + litresInTheTank > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    numberOfLines--;
+                    continue;
+                }
+                else
+                {
+                    litresInTheTank += litresOfWater;
+                    numberOfLines--;
+                }
+
+            }
+            Console.WriteLine(litresInTheTank);
+        }
+    }
+}
