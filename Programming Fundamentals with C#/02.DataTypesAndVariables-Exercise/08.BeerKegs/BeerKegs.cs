4f70c43c4aaef0c32cb1cd2eec64209ef27f9370
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-03 17:40:18 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/08.BeerKegs/BeerKegs.cs
@@ -0,0 +1,30 @@
+﻿namespace _08.BeerKegs
+{
+    internal class BeerKegs
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            double biggestVolume = double.MinValue;
+            string biggestKeg = String.Empty;
+            while (numberOfLines > 0)
+            {
+                double volume = 0;
+                string kegModel = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                volume = Math.PI * Math.Pow(radius , 2) * height;
+                if (volume > biggestVolume)
+                {
+                    biggestVolume = volume;
+                    biggestKeg = kegModel;
+                    numberOfLines--;
+                    continue;
+                }
+                numberOfLines--;
+                
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
