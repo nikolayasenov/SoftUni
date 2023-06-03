d45f506292968585357089505302987fe8731c63
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-03 17:38:58 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/05.PrintPartOfTheASCIITable/PrintPartOfTheASCIITable.cs
@@ -0,0 +1,17 @@
+﻿namespace _05.PrintPartOfTheASCIITable
+{
+    internal class PrintPartOfTheASCIITable
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            char currentChar = ' ';
+            for (int i = start; i <= end; i++)
+            {
+                currentChar = (char)i;
+                Console.Write($"{currentChar} ");
+            }
+        }
+    }
+}
