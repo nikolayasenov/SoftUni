fa5f2a6a9f6f62bac5980eb52716dffa2571f444
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-03 17:40:41 +0100

    initial commit

---
+++ Programming Fundamentals with C#/02.DataTypesAndVariables-Exercise/09.SpiceMustFlow/SpiceMustFlow.cs
@@ -0,0 +1,29 @@
+﻿namespace _09.SpiceMustFlow
+{
+    internal class SpiceMustFlow
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int daysCount = 0;
+            int totalSpiceExtracted = 0;
+            while (yield >= 100)
+            {
+                totalSpiceExtracted += yield;
+                if (totalSpiceExtracted - 26 < 0)
+                {
+                    break;
+                }
+                totalSpiceExtracted -= 26 ;
+                yield -= 10;
+                daysCount++;
+            }
+            if (totalSpiceExtracted > 26)
+            {
+                totalSpiceExtracted -= 26;
+            }
+            Console.WriteLine(daysCount);
+            Console.WriteLine(totalSpiceExtracted);
+        }
+    }
+}
