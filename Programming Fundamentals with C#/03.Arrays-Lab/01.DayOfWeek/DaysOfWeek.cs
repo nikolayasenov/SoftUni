ed025e2e7a7d5175cc8a3bf00ac4629b0750efa8
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-04 11:27:17 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/01.DayOfWeek/DaysOfWeek.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.DayOfWeek
+{
+    internal class DaysOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = new string[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            int number = int.Parse(Console.ReadLine());
+            if (number >= 1 && number <= 7)
+            {
+                Console.WriteLine(daysOfWeek[number - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
