8d40bfe60a85f42b18c9144c7d2ab1193bea74bb
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-04 11:27:46 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/02.PrintNumbersInReverseOrder/PrintNumbersInReverseOrder.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.PrintNumbersInReverseOrder
+{
+    internal class PrintNumbersInReverseOrder
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
