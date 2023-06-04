5ebf5aa26e30212b2b5228cc65befa70485d4817
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-04 11:28:11 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/03.RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,19 @@
+﻿namespace _03.RoundingNumbers
+{
+    internal class RoundingNumbers
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{numbers[i]} => {currentNumber}");
+            }
+        }
+    }
+}
