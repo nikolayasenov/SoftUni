4c3372bb82be15746391f892e82fbc65776c22eb
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-04 11:28:36 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/04.RverseArrayOfStrings/ReverseArrayOfStrings.cs
@@ -0,0 +1,17 @@
+﻿namespace _04.RverseArrayOfStrings
+{
+    internal class ReverseArrayOfStrings
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console
+                .ReadLine()
+                .Split(' ')
+                .ToArray();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Console.Write(text[i] + " ");
+            }
+        }
+    }
+}
