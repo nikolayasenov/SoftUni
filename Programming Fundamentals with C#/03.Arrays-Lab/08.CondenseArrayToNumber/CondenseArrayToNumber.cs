f8b6f6e99289bdb337e1a28dfdbb5eec10b1f650
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-05 20:34:41 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/08.CondenseArrayToNumber/CondenseArrayToNumber.cs
@@ -0,0 +1,26 @@
+﻿namespace _08.CondenseArrayToNumber
+{
+    internal class CondenseArrayToNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console
+                .ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            while (nums.Length > 1)
+            {
+                int[] condense = new int[nums.Length - 1];
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+
+                    condense[i] = nums[i] + nums[i + 1];
+                }
+                nums = condense;
+            }
+
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
