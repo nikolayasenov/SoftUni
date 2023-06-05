32b9cce1221bb8551985ab83cfcd7b1ae218629a
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-05 20:33:15 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/05.SumEvenNumbers/SumEvenNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _05.SumEvenNumbers
+{
+    internal class SumEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
