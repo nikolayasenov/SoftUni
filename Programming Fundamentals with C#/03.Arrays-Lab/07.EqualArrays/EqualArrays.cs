14b75de4c3a4e0fe66967a8f21629b718ef5d632
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-05 20:34:15 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/07.EqualArrays/EqualArrays.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _07.EqualArrays
+{
+    internal class EqualArrays
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int[] arrayTwo = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+            int differentIndex = 0;
+            bool arraysAreEqual = true;
+            /* Easier dont need two for loops
+             * for (int i = 0; i < arrayOne.Length; i++)
+             {
+                    if(arrayOne[i] != arrayTwo[i])
+             }
+            */
+            for (int i = 0; i < arrayOne.Length && arraysAreEqual == true; i++)
+            {
+                for (int j = i; j < arrayTwo.Length; j++)
+                {
+                    if (arrayOne[i] == arrayTwo[j])
+                    {
+                        sum += arrayOne[i];
+                    }
+                    else
+                    {
+                        differentIndex = i;
+                        arraysAreEqual = false;
+                        break;
+                    }
+                    break;
+                }
+            }
+            if (arraysAreEqual)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {differentIndex} index");
+            }
+        }
+    }
+}
