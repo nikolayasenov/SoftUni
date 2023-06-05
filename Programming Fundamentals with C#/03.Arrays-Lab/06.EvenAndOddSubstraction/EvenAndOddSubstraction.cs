e1932069aa27c927354c9e5dc36d159a72b1b181
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-05 20:33:48 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Lab/06.EvenAndOddSubstraction/EvenAndOddSubstraction.cs
@@ -0,0 +1,28 @@
+﻿namespace _06.EvenAndOddSubstraction
+{
+    internal class EvenAndOddSubstraction
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int sumEven = 0, sumOdd = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEven += numbers[i];
+                }
+                else
+                {
+                    sumOdd += numbers[i];
+                }
+            }
+            int result = sumEven - sumOdd;
+            Console.WriteLine(result);
+        }
+    }
+}
