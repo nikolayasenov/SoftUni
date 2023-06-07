8f7f8135dafa516603dea47508edb8927e41c9a3
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-07 19:10:47 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Exercise/04.ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.ArrayRotation
+{
+    internal class ArrayRotation
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rotations; i++)
+            {
+                array[0] = array[array.Length - 1];
+                array[array.Length - 1] = array[0];
+            }
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
