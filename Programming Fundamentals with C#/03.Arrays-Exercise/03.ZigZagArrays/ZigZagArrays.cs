16c8b25b5b3be486fb3b07b3d36bd0a04b46e0d5
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-07 19:10:12 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Exercise/03.ZigZagArrays/ZigZagArrays.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.ZigZagArrays
+{
+    internal class ZigZagArrays
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            int[] arrayOne = new int[numberOfLines];
+            int[] arrayTwo = new int[numberOfLines];
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                int[] input = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                if (i % 2 == 0)
+                {
+                    arrayOne[i] = input[0];
+                    arrayTwo[i] = input[1];
+                }
+                else
+                {
+                    arrayOne[i] = input[1];
+                    arrayTwo[i] = input[0];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", arrayOne));
+            Console.WriteLine(string.Join(" ", arrayTwo));
+        }
+    }
+}
