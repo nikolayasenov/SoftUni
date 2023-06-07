dec7898115955eb878fb8a9ebf083f6eb3e17b0f
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-07 19:09:13 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Exercise/01.Train/Train.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.Train
+{
+    internal class Train
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+            int[] wagons = new int[numberOfWagons];
+            //int totalPassengers = 0;
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                int passengers = int.Parse(Console.ReadLine());
+                wagons[i] = passengers;
+            }
+            Console.WriteLine(string.Join(" " , wagons));
+            Console.WriteLine(wagons.Sum());
+        }
+    }
+}
