ebd7013b8b88a055f3579d4f67b0f4fb80b0f978
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-11 18:40:09 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Exercise/07.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,39 @@
+﻿namespace _07.MaxSequenceOfEqualElements
+{
+    internal class MaxSequenceOfEqualElements
+    {
+        static void Main(string[] args)
+        {
+            string[] symbols = Console.ReadLine().Split();
+            int bestCount = 0;
+            string bestCountSymbol = "";
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                int count = 1;
+                for (int j = i + 1; j < symbols.Length; j++)
+                {
+                    if (symbols[i] == symbols[j])
+                    {
+                        count++;
+                        if (bestCount < count)
+                        {
+                            bestCount = count;
+                            bestCountSymbol = symbols[i];
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < bestCount; i++)
+            {
+                Console.Write($"{bestCountSymbol} ");
+            }
+        }
+    }
+}
