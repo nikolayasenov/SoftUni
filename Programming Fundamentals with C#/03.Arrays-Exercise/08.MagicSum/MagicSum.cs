d34fe64e96329be7d961f73ec4d5abf7ff03a26e
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-11 18:40:33 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Exercise/08.MagicSum/MagicSum.cs
@@ -0,0 +1,27 @@
+﻿namespace _08.MagicSum
+{
+    internal class MagicSum
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console
+                .ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int sum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentNumber = array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    int nextNumber = array[j];
+                    if (currentNumber + nextNumber == sum)
+                    {
+                        Console.WriteLine($"{currentNumber} {nextNumber}");
+                    }
+                }
+            }
+        }
+    }
+}
