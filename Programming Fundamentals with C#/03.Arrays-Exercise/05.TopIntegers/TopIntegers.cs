4c534dfe56f18192897ad0b928324cdf587d921d
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-11 18:36:31 +0100

    initial commit

---
+++ Programming Fundamentals with C#/03.Arrays-Exercise/05.TopIntegers/TopIntegers.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.TopIntegers
+{
+    internal class TopIntegers
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isTopInteger = true;
+                int currentNumber = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    int nextNum = array[j];
+                    if (nextNum >= currentNumber)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+
+                }
+
+                if (isTopInteger)
+                {
+                    Console.Write(currentNumber + " ");
+                }
+            }
+        }
+    }
+}
