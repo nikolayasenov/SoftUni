ed20b1891e352d611f8193cbc1827b49df415790
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-16 20:51:16 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/06.CalculateRectangleArea/CalculateRectangleArea.cs
@@ -0,0 +1,17 @@
+﻿namespace _06.CalculateRectangleArea
+{
+    internal class CalculateRectangleArea
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetRectangleArea(width, height);
+            Console.WriteLine(area);
+        }
+        static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
