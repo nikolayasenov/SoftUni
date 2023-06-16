5143f3410ad651ac22ffc4bc1f3b4e550eaf9111
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-16 20:50:53 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/05.Orders/Orders.cs
@@ -0,0 +1,34 @@
+﻿namespace _05.Orders
+{
+    internal class Orders
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            CalculatePrice(product, quantity);
+        }
+
+        private static void CalculatePrice(string product, int quantity)
+        {
+            double price = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    price = 1.50 * quantity;
+                    break;
+                case "water":
+                    price = 1.0 * quantity;
+                    break;
+                case "coke":
+                    price = 1.40 * quantity;
+                    break;
+                case "snacks":
+                    price = 2.00 * quantity;
+                    break;
+            }
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
