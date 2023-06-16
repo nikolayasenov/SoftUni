c9ba023963453d3e18dd9125d107dfdf251dc9a9
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-16 20:50:29 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/04.PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,43 @@
+﻿namespace _04.PrintingTriangle
+{
+    internal class PrintingTriangle
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            PrintTriangle(input);
+            
+        }
+        static void PrintTriangle(int input)
+        {
+            PrintTriangleTopPart(input);
+            PrintBottomTrianglePart(input - 1);
+        }
+
+        private static void PrintBottomTrianglePart(int input)
+        {
+            for (int row = input; row >= 1; row--)
+            {
+                PrintTriangleRow(row);
+            }
+        }
+
+        private static void PrintTriangleRow(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(col + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void PrintTriangleTopPart(int input)
+        {
+            for (int row = 1; row <= input; row++)
+            {
+                PrintTriangleRow(row);
+            }
+        }
+    }
+}
