cb95b8c1ae8d407d20d9b2a4be66e8e4c65dc327
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-16 20:48:11 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/01.SignOfIntegerNumbers/SignOfIntegerNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.SignOfIntegerNumbers
+{
+    internal class SignOfIntegerNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            CheckNumber(number);
+        }
+        static void CheckNumber(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive. ");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero. ");
+            }
+        }
+    }
+}
