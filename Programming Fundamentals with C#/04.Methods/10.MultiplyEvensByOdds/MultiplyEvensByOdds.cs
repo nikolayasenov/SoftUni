ee60293aeb1decd746af46bfebb90ef75af9e9e0
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-18 20:09:45 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/10.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,47 @@
+﻿namespace _10.MultiplyEvensByOdds
+{
+    internal class MultiplyEvensByOdds
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            int result = GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int evenSum = 0;
+            while (number > 0)
+            {
+                int currN = number % 10;
+                number /= 10;
+                if (currN % 2 == 0)
+                {
+                    evenSum += currN;
+                }
+            }
+            return evenSum;
+
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int oddSum = 0;
+            while (number > 0)
+            {
+                int currN = number % 10;
+                number /= 10;
+                if (currN % 2 != 0)
+                {
+                    oddSum += currN;
+                }
+            }
+            return oddSum;
+
+        }
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int totalSum = GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+            return totalSum;
+        }
+    }
+}
