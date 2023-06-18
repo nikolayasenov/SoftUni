b01c7f875c2dc0bf81216c3c44015e48b7531cf6
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-18 20:08:54 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/08.MathPower/MathPower.cs
@@ -0,0 +1,17 @@
+﻿namespace _08.MathPower
+{
+    internal class MathPower
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = PowerOfNumber(number, power);
+            Console.WriteLine(result);
+        }
+        static double PowerOfNumber(double number, int power)
+        {
+            return  Math.Pow(number, power);
+        }
+    }
+}
