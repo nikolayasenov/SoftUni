2cda73590a805da8429887e3dc5ff5c35912c7ee
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-18 20:08:25 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/07.RepeatString/RepeatString.cs
@@ -0,0 +1,19 @@
+﻿namespace _07.RepeatString
+{
+    internal class RepeatString
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            RepeatStrings(str, n);
+        }
+        private static void RepeatStrings(string str, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(str);
+            }
+        }
+    }
+}
