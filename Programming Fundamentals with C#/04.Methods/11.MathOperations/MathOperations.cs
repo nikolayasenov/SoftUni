56998f39cb7c67f63ff8424a6df265367d91cab8
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-18 20:10:07 +0100

    initial commit

---
+++ Programming Fundamentals with C#/04.Methods/11.MathOperations/MathOperations.cs
@@ -0,0 +1,39 @@
+﻿namespace _11.MathOperations
+{
+    internal class MathOperations
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int numberTwo = int.Parse(Console.ReadLine());
+            double result = Calculate(numberOne, @operator, numberTwo);
+            Console.WriteLine(result);
+
+        }
+        static double Calculate(int numberOne, string @operator, int numberTwo)
+        {
+            double result = 0;
+            if (numberTwo !=0 )
+            {
+                switch(@operator)
+                {
+                case "/":
+                    return numberOne / numberTwo;
+                    break;
+                case "*":
+                    return numberOne * numberTwo;
+                    break;
+                case "+":
+                    return numberOne + numberTwo;
+                    break;
+                case "-":
+                    return numberOne - numberTwo;
+                }
+            }
+            return result;
+
+
+        }
+    }
+}
