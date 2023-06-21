a1556f98f7530d5d08673f87f24a933ae79a77c2
Nikolay Asenov <nikolay.as.asenov@gmail.com>
2023-06-21 21:42:30 +0100

    initial commit

---
+++ Programming Fundamentals with C#/05.Lists-Lab/02.GaussTrick/GaussTrick.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.GaussTrick
+{
+    internal class GaussTrick
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                int first = list[i];
+                int second = list[list.Count - i - 1];
+                int sum = first + second;
+                result.Add(sum);
+            }
+
+            if (list.Count % 2 != 0)
+            {
+                result.Add(list[list.Count / 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
